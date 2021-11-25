333079b22efa081eaff85ee9d1c7dea6a1a129c6
immijimmi <immijimmi1@gmail.com>
2021-11-25 23:47:22 +0000

    Tweaks to Audio, added import statement for AudioHandler (main AudioHandler file still to be added)

--- untitledbrawler/cls/audio/audio.py
+++ untitledbrawler/cls/audio/audio.py
@@ -1,28 +1,46 @@
 from pygame import mixer
 
+from weakref import ref
+
+from ..data import AudioHandler
 from .constants import AudioStatuses
 
 
 class Audio:
+    """
+    Each Audio object encapsulates one pygame Sound
+    """
+
     def __init__(
-            self, handler, file_name: str, volume: float = 1.0,
-            status: str = AudioStatuses.PLAYING, full_fade_ms: int = 1000
+            self, parent: "Entity.with_extensions(Animated)", file_name: str,
+            volume: float = 1.0, status: str = AudioStatuses.PLAYING,
+            full_fade_ms: int = 1000
     ):
-        self.volume = volume
+        self.volume = volume  # No support for stereo volume as of yet
         self.status = status
         self.full_fade_ms = full_fade_ms  # Dictates the time a fade from 0% to 100% or vice versa would take.
 
         self._file_name = file_name
-        self._handler = handler
+        self._parent = ref(parent)  # Weakref so that it does not prevent parent object being garbage collected
         self._is_paused = False
 
         self._channel = mixer.Channel(hash(self))
-        self._sound = mixer.Sound(self._file_name)
-        if self.status == AudioStatuses.FADING_IN:
-            self._sound.set_volume(0)
+        self._sound = AudioHandler.get_sound(self)
+        if self.status != AudioStatuses.PLAYING:
+            self._channel.set_volume(0)
+
+        AudioHandler.add(self)
 
         self._channel.play(self._sound)
-        self.update(0)
+        self.update(0)  # AudioHandler will make any successive calls to .update()
+
+    @property
+    def parent(self) -> "Entity.with_extensions(Animated)":
+        return self._parent()
+
+    @property
+    def file_name(self) -> str:
+        return self._file_name
 
     def update(self, elapsed_ms: int):
         # Pausing logic
@@ -40,35 +58,35 @@ class Audio:
             pass  # Status is handled above
 
         elif self.status == AudioStatuses.PLAYING:
-            if self._sound.get_volume() != self.volume:
-                self._sound.set_volume(self.volume)
+            if self._channel.get_volume() != self.volume:
+                self._channel.set_volume(self.volume)
 
         elif self.status == AudioStatuses.FADING_IN:
-            if (curr_volume := self._sound.get_volume()) != self.volume:
+            if (curr_volume := self._channel.get_volume()) != self.volume:
                 volume_diff = self.volume - curr_volume
                 max_fade = min(1.0, elapsed_ms / self.full_fade_ms)
 
                 positive_capped_fade = min(max_fade, volume_diff)
                 capped_fade = max(-max_fade, positive_capped_fade)  # Capped in both positive and negative directions
 
-                self._sound.set_volume(curr_volume + capped_fade)
+                self._channel.set_volume(curr_volume + capped_fade)
 
             if curr_volume == self.volume:
                 self.status = AudioStatuses.PLAYING
 
         elif self.status == AudioStatuses.FADING_OUT:
-            if (curr_volume := self._sound.get_volume()) != 0:
+            if (curr_volume := self._channel.get_volume()) != 0:
                 volume_diff = 0 - curr_volume
                 max_fade = min(1.0, elapsed_ms / self.full_fade_ms)
 
                 positive_capped_fade = min(max_fade, volume_diff)
                 capped_fade = max(-max_fade, positive_capped_fade)  # Capped in both positive and negative directions
 
-                self._sound.set_volume(curr_volume + capped_fade)
+                self._channel.set_volume(curr_volume + capped_fade)
 
             if curr_volume == 0:
-                self._stop()
                 self.status = AudioStatuses.STOPPED
+                self._stop()
 
         elif self.status == AudioStatuses.STOPPED:
             self._stop()
@@ -80,4 +98,4 @@ class Audio:
         self._sound.stop()
         self._channel.stop()
 
-        self._handler.remove(self)
+        AudioHandler.remove(self)
--- untitledbrawler/cls/data/__init__.py
+++ untitledbrawler/cls/data/__init__.py
@@ -1,2 +1,3 @@
 from .animationhandler import AnimationHandler
 from .statehandler import StateHandler
+from .audiohandler import AudioHandler
