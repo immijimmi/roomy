10b2c3cb7c0e77b3eb96a1fd697ac03f841dbf72
immijimmi <imranhamid99@msn.com>
2022-06-14 03:25:56 +0100

    Structural updates to the Game class in preparation for converting repo to solely a game engine as opposed to a specific game's project

--- untitledbrawler/game.py
+++ untitledbrawler/game.py
@@ -1,78 +1,61 @@
-from managedstate import State
-from managedstate.extensions import Listeners, Registrar
 import pygame
 
-from json import loads
-from typing import Type
+from typing import Type, Sequence, Tuple
 
 from .cls import *
 from .constants import Constants
 
 
 class Game:
-    resolution = (1280, 720)
     fps = 144
 
-    def __init__(self):
+    def __init__(self, resolution: Sequence[int], initial_screen: Screen):
+        self._resolution = (resolution[0], resolution[1])
+        self._screen = initial_screen
         self._observer_manager = ObserverManager
 
         pygame.mixer.pre_init(frequency=Constants.AUDIO_FREQUENCY)
         pygame.init()
         pygame.mixer.init()
 
-        self._window = pygame.display.set_mode(self.resolution)
+        self._window = pygame.display.set_mode(self._resolution)
         self._clock = pygame.time.Clock()
 
-        self._screen = None
+    @property
+    def resolution(self) -> Tuple[int, int]:
+        return self._resolution
 
-        self._init_screen()
-        self.run()
+    @property
+    def screen(self) -> Screen:
+        return self._screen
+
+    @screen.setter
+    def screen(self, value: Screen):
+        old_screen = self._screen
+        self._screen = value
+
+        self._observer_manager.on_change_screen(old_screen, value)
 
     @property
     def observer_manager(self) -> Type[ObserverManager]:
         return self._observer_manager
 
     @property
-    def screen(self) -> Screen:
-        return self._screen
-
-    @property
     def window(self):
         return self._window
 
-    def change_screen(self, new_screen: Screen) -> None:
-        old_screen = self._screen
-        self._screen = new_screen
-
-        self._observer_manager.on_change_screen(old_screen, new_screen)
-
-    def run(self) -> None:
+    def start(self) -> None:
         while True:
-            self.update()
-            self.render()
+            self._update_screen()
+            self._render_screen()
 
-    def update(self) -> None:
+    def _update_screen(self) -> None:
         elapsed_ms = self._clock.tick(self.fps)
         events = list(pygame.event.get())  # Events are passed in a list so that they can be consumed if necessary
 
         self._screen.update(elapsed_ms, events)
 
-    def render(self) -> None:
+    def _render_screen(self) -> None:
         updated_rects = self._screen.render(self._window)
 
         pygame.display.update(updated_rects)
-
-    def _init_screen(self):
-        ##### TODO: Replace with logic which loads a menu Screen
-
-        state_extended_class = State.with_extensions(Registrar, Listeners)
-        save_state = state_extended_class()
-        StateHandler.register_paths(save_state)
-
-        try:
-            with open("save/save1.json", "r") as file:
-                save_state.set(loads(file.read()))
-        except FileNotFoundError:
-            StateHandler.generate_new_save_data(save_state)
-
-        self.change_screen(World(self, save_state))
