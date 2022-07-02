b51b6b70e271beedba8c163a2951d8cc56fa8053
immijimmi <imranhamid99@msn.com>
2022-07-02 11:41:12 +0100

    Minor refactoring and comment edits in Animated

--- roomy/cls/ext/animated.py
+++ roomy/cls/ext/animated.py
@@ -14,20 +14,24 @@ class Animated(Extension):
         Extension._wrap(target_cls, "__init__", Animated.__wrap_init)
         Extension._wrap(target_cls, "update", Animated.__wrap_update)
 
-        Extension._set(target_cls, "generate_default_animation", Animated.__generate_default_animation)
+        Extension._set(target_cls, "generate_animation", Animated.__generate_animation)
         Extension._set(target_cls, "apply_animation", Animated.__apply_animation)
 
-    def __generate_default_animation(self) -> Animation:
+    def __generate_animation(self) -> Animation:
         """
         Must be overridden.
-        Should return a default animation for this entity
+        Should return the correct animation for this entity based on its current state.
+        Note that event-driven rather than state-driven animations can be applied without the use of this method,
+        by directly calling .apply_animation() (and passing it a new Animation object) instead
         """
 
         raise NotImplementedError
 
     def __apply_animation(self, animation: Animation) -> None:
         """
-        Workaround method since objectextensions does not currently support binding properties
+        Workaround method since objectextensions does not currently support binding properties.
+        Named 'apply' rather than 'set' because it is not guaranteed that the provided animation will be set,
+        if it is lower priority than the current animation
         """
 
         if animation.priority >= self._animation.priority:
@@ -35,7 +39,7 @@ class Animated(Extension):
 
     def __wrap_init(self, *args, **kwargs):
         yield
-        Extension._set(self, "_animation", self.generate_default_animation())
+        Extension._set(self, "_animation", self.generate_animation())
 
     def __wrap_update(self, elapsed_ms, events):
         yield
