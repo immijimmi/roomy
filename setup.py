470f3344ba35bb736a63765ba647f9623bc167b6
immijimmi <immijimmi1@gmail.com>
2022-10-20 01:24:38 +0100

    Updated dependency

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         "pygame~=2.0.1",
         "managedstate~=5.0.0",
-        "recurfaces~=1.0.6",
+        "recurfaces~=1.0.7",
         "objectextensions~=2.0.1"
     ],
     classifiers=[
