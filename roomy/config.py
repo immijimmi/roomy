79f9acbbe863c648502ced9d5f28646381009f1c
immijimmi <immijimmi1@gmail.com>
2022-11-13 13:15:45 +0000

    Tweaked config values

--- roomy/config.py
+++ roomy/config.py
@@ -6,7 +6,7 @@ from .renderables import RenderableHitboxTag
 
 
 class Config:
-    TICK_RATE: float = 144  # Number of game ticks per second. A value of 0 indicates unlimited tick rate (must be >=0)
+    TICK_RATE: float = 120  # Number of game ticks per second. A value of 0 indicates unlimited tick rate (must be >=0)
     """
     Please note that while unlimited tick rate is supported, any parts of the game state
     which are updated per tick may behave differently in a given period of real time at different tick rates.
@@ -25,4 +25,4 @@ class Config:
         attribute.value for attribute in RenderableHitboxTag
     )
 
-    ANIMATION_DEFAULT_FPS: float = 50
+    ANIMATION_DEFAULT_FPS: float = 24
