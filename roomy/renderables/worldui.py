b8ec13e522769c8d5038799a32a9f320a9d8ad3d
immijimmi <immijimmi1@gmail.com>
2023-02-20 16:09:00 +0000

    Added surface param back into WorldUi (unlike Room, it's not currently auto-generated by pulling data from the game state)

--- roomy/renderables/worldui.py
+++ roomy/renderables/worldui.py
@@ -1,3 +1,5 @@
+from pygame import Surface
+
 from abc import ABC
 from typing import Optional, Tuple
 
@@ -7,7 +9,8 @@ from .renderable import Renderable
 class WorldUi(Renderable, ABC):
     def __init__(
             self, parent: "World", ui_layer_id: str,
-            render_position: Optional[Tuple[float, float]] = None, priority: float = 1
+            surface: Optional[Surface] = None, render_position: Optional[Tuple[float, float]] = None,
+            priority: float = 1
     ):
         super().__init__(parent.game, parent=parent, render_position=render_position, priority=priority)
         # Note that priority must be a float value > 0 here, in order to render above the current Room
