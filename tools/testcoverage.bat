98e9b97967e3780d734ef203b51f3a72a45ce23f
immijimmi <immijimmi1@gmail.com>
2023-06-24 05:55:25 +0100

    Updated tool

--- tools/testcoverage.bat
+++ tools/testcoverage.bat
@@ -1,8 +1,8 @@
-:: Requires pytest and coverage to be installed
+:: Requires pytest~=7.4.0 and coverage~=7.2.7 to be installed
 :: This script will run any test files it discovers in the directory and provide a breakdown of code coverage
 
 cd ..
-coverage run -m --source=. --omit="test\*,setup.py" py.test -v
+coverage run -m --source=. --omit="test\*,setup.py" pytest -v
 coverage report
 
 pause
